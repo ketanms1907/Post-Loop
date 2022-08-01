4fe30e8c4d2d52c5fe4c0ff59f9d27cd6c539856
ketanms1907 <110393942+ketanms1907@users.noreply.github.com>
2022-08-01 17:57:02 +0530

    Update post-loop.php

--- post-loop.php
+++ post-loop.php
@@ -1,8 +1,23 @@
-<?php $args = array('posts_per_page' => '',
+<div class="row">
+<?php
+$args = array('posts_per_page' => 4,
+'category_name' => 'latest',
 'post_status' => 'publish',
-'post_type' => '',
+'post_type' => 'post',
 'order' => 'DESC' );
 $posts_array = get_posts( $args );
 foreach ( $posts_array as $post ) : setup_postdata( $post ); ?>
+<div class="col-md-3 col-sm-6 col-xs-6">
+<div class="news-box">
+<?php
+if ( has_post_thumbnail() ) {
+echo get_the_post_thumbnail();
+}
+?>
+<h4> <?php the_title(); ?> </h4>
+<p><?php the_content(); ?> </p> <p><?php echo get_post_meta(get_the_ID(), "Price", true); ?></p>
+</div>
+</div>
 <?php endforeach;
 wp_reset_postdata();?>
+</div>
